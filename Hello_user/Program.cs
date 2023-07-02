22138e5c6c38e7239ad23255ff74a2361d5645d6
Nikeguga <nikeguga@gmail.com>
2023-07-02 16:29:52 +0300

    special guest

---
+++ Hello_user/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter your name");
+string username = Console.ReadLine();
+
+if (
+    username.ToLower == "nikita"
+    )
+
+{
+    Console.Write ("Greetings my lord ");
+    Console.Write (username);
+}
+
+else
+
+{
+    Console.Write ("Hello, ");
+    Console.Write (username);
+    Console.Write (". It is a pitty that you are not Nikita");
+}
