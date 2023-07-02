3b117ed987417ac32c325c7968832ba8f383d1b9
Nikeguga <nikeguga@gmail.com>
2023-07-02 16:21:37 +0300

    so good in user names

--- Hello user/Program.cs
+++ Hello user/Program.cs
@@ -1,4 +1,17 @@
 ﻿Console.WriteLine("Enter your name");
 string username = Console.ReadLine();
-Console.WriteLine ("Hello,");
-Console.WriteLine (username);
+
+if (username == "Nikita")
+
+{
+    Console.Write ("Greetings my lord ");
+    Console.Write (username);
+}
+
+else
+
+{
+    Console.Write ("Hello, ");
+    Console.Write (username);
+    Console.Write (". It is a pitty that you are not Nikita");
+}
