3a1ec287f7c873bd16729b130522167d8123ee23
Nikeguga <nikeguga@gmail.com>
2023-07-02 16:07:04 +0300

    ranomized changed

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,5 +1,5 @@
-﻿int numberA = new Random().Next(1, 112);
+﻿int numberA = new Random().Next(1, 115);
 Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 112);
+int numberB = new Random().Next(1, 115);
 Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
