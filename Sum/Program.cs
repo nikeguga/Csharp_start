db7f00e8dad3c0d9a5948b72c6767ab61559346d
Nikeguga <nikeguga@gmail.com>
2023-07-02 15:59:10 +0300

    randomized sum

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 521;
-int numberB = 185;
+﻿int numberA = new Random().Next(1, 112);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 112);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
